8289e19fb7a488b1e04202e6208644b970b2242b
ChehsienLiao33 <chli7363@stu.sjcc.edu>
2026-02-27 00:39:07 -0800

    Add example code and create the menu

---
+++ chavvicalc/src/main/java/com/chavvicalc/ChavviCalcExampleApp.java
@@ -0,0 +1,89 @@
+package com.chavvicalc;
+
+import java.util.*;
+
+/*
+ * chavvi calc calculator
+ */
+public class ChavviCalcExampleApp {
+
+  public static void main(String[] args) {
+    Scanner scan = new Scanner(System.in);
+    Character command = '_';
+
+    // loop until user quits
+    while (command != 'q') {
+      printMenu();
+      System.out.print("Enter a command: ");
+      command = menuGetCommand(scan);
+
+      executeCommand(scan, command);
+    }
+
+    scan.close();
+  }
+
+  //
+  // menu functions
+  //
+  private static void printMenuLine() {
+    System.out.println(
+        "----------------------------------------------------------");
+  }
+
+  private static void printMenuCommand(Character command, String desc) {
+    System.out.printf("%s\t%s\n", command, desc);
+  }
+
+  // prints the menu
+  public static void printMenu() {
+    printMenuLine();
+    System.out.println("Chavvi Calc");
+    printMenuLine();
+
+    // Display A and B with 3 significant digits
+    System.out.printf("A = %.3f\tB = %.3f\n", 0.0, 0.0);
+    printMenuLine();
+
+    printMenuCommand('a', "Enter a value for A");
+    printMenuCommand('b', "Enter a value for B");
+    printMenuCommand('+', "Add");
+    printMenuCommand('-', "Subtract");
+    printMenuCommand('*', "Multiply");
+    printMenuCommand('/', "Divide");
+    printMenuCommand('c', "Clear");
+    printMenuCommand('q', "Quit");
+
+    printMenuLine();
+  }
+
+  // get first character from input
+  private static Character menuGetCommand(Scanner scan) {
+    Character command = '_';
+
+    String rawInput = scan.nextLine();
+
+    if (rawInput.length() > 0) {
+      rawInput = rawInput.toLowerCase();
+      command = rawInput.charAt(0);
+    }
+
+    return command;
+  }
+
+  // calculator functions
+  private static Boolean executeCommand(Scanner scan, Character command) {
+    Boolean success = true;
+
+    switch (command) {
+      case 'q':
+        System.out.println("Thank you for using Chavvi Calc");
+        break;
+      default:
+        System.out.println("ERROR: Unknown commmand");
+        success = false;
+    }
+
+    return success;
+  }
+}
